b4e46e9da29d3fe6179052127d3396c77afe654f
sunnysingania7 <sunnysingania7@live.com>
2025-06-13 19:32:50 +0530

    Create logger.py

---
+++ web-vuln-scanner/scanner/logger.py
@@ -0,0 +1,10 @@
+# scanner/logger.py
+
+def log_vulnerability(vuln_type, url payload, severity):
+    """Return a dictionary of the found vulnerability"""
+    return {
+        "type": vuln_type,
+        "url": url,
+        "payload": payload,
+        "serverity": serverity
+    }
